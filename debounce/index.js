cb3df46b689b854a4632faf8de8cbab2f67cf118
王思杰 <wsj_simon@163.com>
2019-02-28 14:35:54 +0800

    add debounce and throttle

---
+++ debounce/index.js
@@ -0,0 +1,24 @@
+function debounce (func, wait = 500) {
+    let timmer = null
+
+    return function (props) {
+        if (timmer) {
+            clearTimeout(timmer)
+        }
+        timmer = setTimeout(() => {
+            func(props)
+            timmer = null
+        }, wait)
+    }    
+}
+
+const inputEl = document.querySelector('#testInput')
+
+const logDounce = debounce(value => {
+    console.log('log', value)
+}, 500)
+inputEl.addEventListener('input', function (e) {
+    const value = e.target.value
+    
+    logDounce(value)
+})
